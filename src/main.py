a6f98adae2e2b0a1ef01d533ec0749cb3b4d2b69
Jaydrey <jaribwetshi64@gmail.com>
2024-12-26 17:04:21 +0300

    feat(main): create the send to twilio and send_session_update async function

--- src/main.py
+++ src/main.py
@@ -13,12 +13,14 @@ from twilio.twiml.voice_response import VoiceResponse, Connect, Say, Stream
 
 # settings
 from .settings import (
+    ALLOWED_HOST,
     INITIAL_WAIT_MESSAGE,
     OPENAI_API_KEY,
     OPENAI_REALTIME_API_URL,
     PORT,
     LOG_EVENT_TYPES,
-    SYSTEM_MESSAGE
+    SYSTEM_MESSAGE,
+    VOICE
 )
 
 app = FastAPI()
@@ -73,12 +75,80 @@ async def media_stream(websocket: WebSocket):
                     if data["event"] == "start":
                         stream_sid = data["start"]["streamSid"]
                         print(f"Incoming stream has started {stream_sid}")
-                        audio = data["audio"]
-                        await openai_ws.send(audio)
+                    elif data["event"] == "media" and openai_ws.open:
+                        audio_append = {
+                            "type": "input_audio_buffer.append",
+                            "audio": data["media"]["payload"]
+                        }
+                        await openai_ws.send(json.dumps(audio_append))
                     
             except WebSocketDisconnect:
                 print("Twilio disconnected")
+                if openai_ws.open:
+                    await openai_ws.close()
                 return
             except Exception as e:
                 print(f"Error receiving from Twilio: {e}")
                 return
+            
+        async def send_to_twilio():
+            nonlocal stream_sid
+            try:
+                async for openai_message in openai_ws:
+                    data:dict[str, Any] = json.loads(openai_message)
+                    if data["type"] in LOG_EVENT_TYPES:
+                        print(f"Received event: {data['type']}, {data=}")
+                    
+                    if data["type"] == "session.updated":
+                        print(f"Session updated: {data=}")
+                    
+                    if data["type"] == "response.audio.delta" and data.get("delta"):
+                        try:
+                            audio_payload = base64.b64encode(base64.b64decode(data["delta"])).decode()
+                            audio_delta = {
+                                "event": "media",
+                                "streamSid": stream_sid,
+                                "media": {
+                                    "payload": audio_payload,
+                                    
+                                }
+                            }
+                            await websocket.send_json(audio_delta)
+                        except Exception as e:
+                            print(f"Error processing audio: {e}")
+                            return
+                        
+            except WebSocketDisconnect:
+                print("OpenAI disconnected")
+                if websocket.client_state != 3:
+                    await websocket.close()
+                return
+            except Exception as e:  
+                print(f"Error sending to Twilio: {e}")
+                return
+            
+        await asyncio.gather(receive_from_twilio(), send_to_twilio())
+
+
+async def send_session_update(openai_ws):
+    session_update = {
+        "type": "session.update",
+        "session": {
+            "turn_detection": {
+                "type": "server_vad"
+            },
+            "input_audio_format": "g711_ulaw",
+            "output_audio_format": "g711_ulaw",
+            "voice": VOICE,
+            "instructions": SYSTEM_MESSAGE,
+            "modalities": ["text", "audio"],
+            "temperature": 0.8,
+        },
+        
+    }
+    await openai_ws.send(json.dumps(session_update))
+    print("Sent session update")
+
+
+
+
